d3dea88e092619a1dd85917192d0f70926386128
TRIVAIT <dimitmapper@gmail.com>
2026-02-14 14:49:22 +0300

    initial commit

---
+++ src/main/java/org/trivait/blackwhitepalegarden/BlackWhitePaleGarden.java
@@ -0,0 +1,17 @@
+package org.trivait.blackwhitepalegarden;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BlackWhitePaleGarden implements ClientModInitializer {
+	public static final String MOD_ID = "blackwhitepalegarden";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
